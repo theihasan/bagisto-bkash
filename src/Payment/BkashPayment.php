249d6d4f6feb71800c76bd98991be661b2340e0e
Md Abul Hassan <imabulhasan99@gmail.com>
2025-03-30 11:08:43 +0600

    add BkashPayment class for handling bKash payment processing

---
+++ src/Payment/BkashPayment.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Payment;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Webkul\BkashPayment\Services\BkashPaymentService;
+use Webkul\BkashPayment\Exceptions\PaymentException;
+use Webkul\Payment\Payment\Payment;
+use function Webkul\BkashPayment\Payment\asset;
+use function Webkul\BkashPayment\Payment\cart;
+use function Webkul\BkashPayment\Payment\route;
+use function Webkul\BkashPayment\Payment\session;
+
+class BkashPayment extends Payment
+{
+    /**
+     * Payment method code
+     *
+     * @var string
+     */
+    protected $code = 'bkash_payment';
+
+    /**
+     * Create a new payment method instance.
+     *
+     * @param BkashPaymentService $bkashPaymentService
+     * @return void
+     */
+    public function __construct(
+        protected BkashPaymentService $bkashPaymentService
+    ) {
+    }
+
+    /**
+     * Get redirect URL
+     *
+     * @return string
+     */
+    public function getRedirectUrl()
+    {
+        try {
+            $cart = cart()->getCart();
+            $paymentData = $this->bkashPaymentService->createPayment($cart);
+
+            Log::info('bKash payment initiated:', $paymentData);
+
+            return $paymentData['bkashURL'];
+        } catch (PaymentException $e) {
+            Log::error('bKash payment initiation error:', [
+                'message' => $e->getMessage(),
+                'trace' => $e->getTraceAsString()
+            ]);
+
+            session()->flash('error', $e->getMessage());
+            return route('shop.checkout.cart.index');
+        } catch (\Exception $e) {
+            Log::error('Unexpected error in bKash payment initiation:', [
+                'message' => $e->getMessage(),
+                'trace' => $e->getTraceAsString()
+            ]);
+
+            session()->flash('error', 'An error occurred while setting up the payment. Please try again or contact support.');
+            return route('shop.checkout.cart.index');
+        }
+    }
+
+    /**
+     * Process the payment callback
+     *
+     * @param Request $request
+     * @return mixed
+     */
+    public function handleCallback(Request $request)
+    {
+        return $this->bkashPaymentService->processCallback($request);
+    }
+
+    /**
+     * Get payment method image
+     *
+     * @return string
+     */
+    public function getImage(): string
+    {
+        $url = $this->getConfigData('image');
+        return $url ? Storage::url($url) : asset('vendor/bkash_payment/images/bkash-logo.png');
+    }
+}
