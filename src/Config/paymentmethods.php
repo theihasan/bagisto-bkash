7d98440d0406c2fa55bd4a8c23573c6adbd8d9c7
Md Abul Hassan <imabulhasan99@gmail.com>
2025-03-30 11:01:59 +0600

    add bKash payment method configuration

---
+++ src/Config/paymentmethods.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'bkash_payment' => [
+        'code'        => 'bkash_payment',
+        'title'       => 'bKash Payment',
+        'description' => 'bKash Payment Gateway',
+        'class'       => 'Webkul\BkashPayment\Payment\BkashPayment',
+        'active'      => true,
+        'sort'        => 5,
+    ],
+];
